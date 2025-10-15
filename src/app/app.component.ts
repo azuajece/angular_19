2807bfa0b396a011d2d46c1eb9b94bd064cf75ec
Carlos Azuaje <carlos.azuaje@accenture.com>
2025-10-15 09:59:23 -0300

    3ro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.less"
 })
 export class AppComponent {
-  title = 'prueba';
+  x = 3;
+  title = "Angular 19";
 }
